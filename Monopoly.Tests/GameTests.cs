b06da77bd8ca296bbcf1594d5cdeec90db60e81b
Andrea Angella <andrea.angella@red-gate.com>
2017-01-09 22:48:52 +0000

    Implement first player roll the dices

--- Monopoly.Tests/GameTests.cs
+++ Monopoly.Tests/GameTests.cs
@@ -47,5 +47,25 @@ namespace Monopoly.Tests
 
             Assert.That(game.CurrentPlayer, Is.EqualTo(player1));
         }
+
+
+        [Test]
+        public void FirstPlayer_RollDice()
+        {
+            var player1 = new Player("Player1");
+            var player2 = new Player("Player2");
+            var dice1 = new Dice(1);
+            var dice2 = new Dice(3);
+
+            var game = new Game();
+            game.AddDice(dice1);
+            game.AddDice(dice2);
+            game.AddPlayer(player1);
+            game.AddPlayer(player2);
+
+            game.Play();
+
+            Assert.That(game.DiceValue, Is.EqualTo(4));
+        }
     }
 }
--- Monopoly/Game.cs
+++ Monopoly/Game.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 
 namespace Monopoly
 {
     public class Game
     {
         private readonly List<Player> players = new List<Player>();
+        private readonly List<Dice> dices = new List<Dice>();
+        
         private int currentPlayerIndex;
 
         public void AddPlayer(Player player)
@@ -17,6 +21,24 @@ namespace Monopoly
         public void Next()
         {
             currentPlayerIndex = (currentPlayerIndex + 1) % players.Count;
+        }
+
+        public void AddDice(Dice dice)
+        {
+            dices.Add(dice);
+        }
+
+        public void Play()
+        {
+            foreach (var dice in dices)
+            {
+                 dice.Roll();          
+            }
+        }
+
+        public int DiceValue
+        {
+            get { return dices.Select(v => v.Value.Value).Sum(); }
         }
     }
 }
