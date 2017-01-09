6b10124d74a7e173984d4a3f09636e061cfbf10a
Andrea Angella <andrea.angella@red-gate.com>
2017-01-09 21:04:51 +0000

    Initial implementation of Dice class

---
+++ Monopoly.Tests/DiceTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Monopoly.Tests
+{
+    public class DiceTests
+    {
+        [Test]
+        public void New()
+        {
+            var dice = new Dice(1, 6);
+
+            Assert.IsNull(dice.Value);
+        }
+
+        [Test]
+        public void Roll()
+        {
+            var dice = new Dice(1, 6);
+            dice.Roll();
+
+            Assert.That(dice.Value, Is.InRange(1, 6));
+        }
+    }
+}
---
+++ Monopoly/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Monopoly
+{
+    public class Dice
+    {
+        private readonly Random random;
+        private readonly int min;
+        private readonly int max;
+
+        public Dice(int min, int max)
+        {
+            random = new Random();
+            this.min = min;
+            this.max = max;
+        }
+
+        public int? Value { get; set; }
+
+        public void Roll()
+        {
+           Value = random.Next(min, max);
+        }
+    }
+}
